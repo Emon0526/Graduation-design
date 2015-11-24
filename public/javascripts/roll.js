310bc768533c4b681ba055b703cb38dc57a9a3de
Emon0526 <465293685@qq.com>
2015-11-24 18:18:08 +0800

    添加轮播图

--- public/javascripts/roll.js
+++ public/javascripts/roll.js
@@ -1,13 +1,3 @@
-$(function() {
-    $('#slides').slidesjs({
-        width: 940,
-        height: 528,
-        navigation: false,
-        play: {
-            active: false,
-            auto: true,
-            interval: 4000,
-            swap: true
-        }
-    });
-});
+$('.carousel').carousel({
+    interval: 2000
+})
