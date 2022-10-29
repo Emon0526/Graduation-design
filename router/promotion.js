cc23d7b52724f76abe61c7a3c550f4a122080777
Emon <doraemon.llg@gmail.com>
2022-10-29 16:15:06 +0800

    boomb(delete): router/promotion.js

--- router/promotion.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var pro = require('../controller/promotion-controller');
-
-router.get('/', pro.renderPromotion);
-
-router.get('/:promotionId', pro.lablePromotion)
-
-module.exports = router;
